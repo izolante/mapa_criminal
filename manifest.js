a228bb8a311d93d89764502d708686b771a6c590
izolante <91175401+izolante@users.noreply.github.com>
2024-08-06 04:40:43 -0300

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "short_name": "App",
+  "name": "Nome Completo da Aplicação",
+  "icons": [
+    {
+      "src": "icono-192x192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "icono-512x512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": "/",
+  "background_color": "#FFFFFF",
+  "display": "standalone",
+  "scope": "/",
+  "theme_color": "#FFFFFF"
+}
